0f594ef739697563bdebd4b886ab4f4ec34e1960
Justin Law <justinlaw31@gmail.com>
2020-08-11 20:43:25 -0700

    fade in on cards
    added js logic to fade in on cards, past scroll distance

--- TickyBot Clone/app.js
+++ TickyBot Clone/app.js
@@ -1,20 +1,31 @@
 //Fade button in on load
-document.addEventListener('DOMContentLoaded', function () {
-  let btnWhite = document.getElementById('btn--white');
-  btnWhite.classList.add('btn--fade');
+document.addEventListener("DOMContentLoaded", function () {
+  let btnWhite = document.getElementById("btn--white");
+  btnWhite.classList.add("btn--fade");
 });
 
 //Smooth scrolling from navigation bar
-const navLinks = document.querySelectorAll('.navigation a');
+const navLinks = document.querySelectorAll(".navigation a");
 
 //apply event listener to all but the last one
 for (let i = 0; i < 3; i++) {
-  navLinks[i].addEventListener('click', navLinkClick);
+  navLinks[i].addEventListener("click", navLinkClick);
 }
 
 function navLinkClick(event) {
   event.preventDefault();
-  document.querySelector(this.getAttribute('href')).scrollIntoView({
-    behavior: 'smooth',
+  document.querySelector(this.getAttribute("href")).scrollIntoView({
+    behavior: "smooth",
   });
 }
+
+//fade card in on scroll using active class
+const cards = document.querySelectorAll(".card");
+
+window.onscroll = function () {
+  if (window.pageYOffset > 1120) {
+    cards.forEach((card) => {
+      card.classList.add("active");
+    });
+  }
+};
